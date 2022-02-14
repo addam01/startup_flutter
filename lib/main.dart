8c2b755b4a30af7cf485be21d12ee510ccd467d3
Addam <addam01@hotmail.com>
2022-02-14 13:19:45 +0800

    Done simple checklist to show checked in another UI

--- lib/main.dart
+++ lib/main.dart
@@ -24,9 +24,15 @@ class MyApp extends StatelessWidget {
     //     ),
     //   ),
     // );
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Startup Name Generator',
-      home: RandomWords(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        )
+      ),
+      home: const RandomWords(),
     );
   }
 }
@@ -40,6 +46,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestion = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
   // @override
   // Widget build(BuildContext context) {
@@ -51,8 +58,48 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+              onPressed: _pushSaved,
+              icon: const Icon(Icons.list),
+              tooltip: 'Saved Suggestions',
+          )
+        ],
       ),
       body: _buildSuggestions(),
+    );
+  }
+
+  //Using Navigator to move to another UI
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context){
+
+          final tiles = _saved.map(
+              (pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              }
+          );
+
+          final divided = tiles.isNotEmpty ? ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+          ).toList() : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided,),
+          );
+        },
+      ),
     );
   }
 
@@ -74,11 +121,27 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+      ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          }
+          else{
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
